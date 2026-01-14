f6cc63671d27ea090e6ea2b6000a29af94de2329
Artin-cell <thebestartin@gmail.com>
2026-01-14 23:32:30 +0300

    minor update

--- src/config_secret_example.h
+++ src/config_secret_example.h
@@ -3,14 +3,6 @@
 #ifndef CONFIG_SECRET_H
 #define CONFIG_SECRET_H
 
-// WiFi настройки
-#define AP_SSID "Your_WiFi_SSID"
-#define AP_PASS "Your_WiFi_Password"
-
-// Google Apps Script URLs
-#define GOOGLE_SCRIPT_URL "https://script.google.com/macros/s/YOUR_SCRIPT_ID/exec"
-#define GOOGLE_SCRIPT_IP_URL "https://script.google.com/macros/s/YOUR_SCRIPT_ID/exec?action=IP&value="
-
 // Другие настройки
 #define NTP_SERVER "pool.ntp.org"
 #define TIMEZONE_OFFSET ____ //Your_UTC  
